3975baf70e29a38f8e21c985e9a6363466c5809d
SamitSaleem <samit.saleem@paysyslabs.com>
2026-07-03 16:44:44 +0500

    feat: feat
    Signed-off-by: SamitSaleem <samit.saleem@paysyslabs.com>

--- dist/tmi/tmi.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class TmiService {
-    private readonly configService;
-    private readonly logger;
-    constructor(configService: ConfigService);
-    processRawString(raw: string): Promise<string>;
-}
